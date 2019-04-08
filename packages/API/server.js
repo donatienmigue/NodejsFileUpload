aff748c3ff802b976a9ba5081e08bd55de97ddf9
miguedonatien <miguedonatien1@gmail.com>
2019-04-08 17:10:13 -0400

    setup api routes

--- packages/API/server.js
+++ packages/API/server.js
@@ -1,9 +1,17 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const uploadRoutes = require('./uploadRoutes');
 
 const PORT = process.env.PORT || 4444;
-
 const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json({ type: 'application/json' }));
+app.use(express.static('public'));
 
+app.get('/api', (req, res) => {
+  res.send('Wellcome Guy');
+});
+app.use('/upload', uploadRoutes);
 app.listen(PORT, (err) => {
   if (err) {
     throw err;
